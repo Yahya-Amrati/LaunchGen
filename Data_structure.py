d7e92e76c8b245326b1effd4cbcefb81e8b0b673
Yahya-Amrati <y.amrati.code@gmail.com>
2025-02-02 12:12:07 +0100

    12/11 Data_struct work

--- Data_structure.py
+++ Data_structure.py
@@ -2,14 +2,25 @@ from dataclasses import dataclass
 import uuid
 
 
+ILLEGAL_CHARS: str = """
+"!"#$%&'()*+,/:;<=>?@[\\]^`{|}~ ¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸
+¹º»¼½¾¿×÷ʰʲʳʷʸⁿ℗℠™Ω℧←↑→↓↔↕✓✔✕✖✗✘☆★♠♣♥♦♪♫⛔⚠️🚀🔥
+💀😊🤖中文日本語हिन्दी"""
+
+
 def generate_random_username() -> str:
-    uuid__name = str(uuid.uuid4())
-    name = []
+    uuid__name: str = str(uuid.uuid4())
+    name: list = []
     for ix, i in enumerate(uuid__name.split("-")):
         # checking if for not letting the username be too long
         # cause max username in minecraft length is 16
         if ix == 15:
             break
+        for x in i.split():
+            if any(ILLEGAL_CHARS in x):
+                i = i.replace(x, "")
+            else:
+                continue
         name.append(i)
     return "".join(name)
 
@@ -17,7 +28,7 @@ def generate_random_username() -> str:
 @dataclass(sorted=True)
 class UserMinecraft:
     name: str = generate_random_username()
-    Offline: bool
+    Offline: bool = True
     Vanilla: bool
     Forge: bool
     Fabric: bool
@@ -25,6 +36,15 @@ class UserMinecraft:
 
     def __init__(self):
         # correct values if possible
+        new_name: list = []
+        for i in self.name:
+            if any(ILLEGAL_CHARS, i):
+                i = ""
+                new_name.append(i)
+            else:
+                new_name.append(i)
+                continue
+        self.name = "".join(new_name)
         if self.Vanilla:
             self.Forge = False
             self.Fabric = False
