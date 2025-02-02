57e9f540a81b2920b708ce70169f753892894aff
Yahya-Amrati <y.amrati.code@gmail.com>
2025-02-02 11:45:09 +0100

    11/44 Data_struct work

--- Data_structure.py
+++ Data_structure.py
@@ -2,17 +2,36 @@ from dataclasses import dataclass
 import uuid
 
 
-@dataclass
+def generate_random_username() -> str:
+    uuid__name = str(uuid.uuid4())
+    name = []
+    for ix, i in enumerate(uuid__name.split("-")):
+        # checking if for not letting the username be too long
+        # cause max username in minecraft length is 16
+        if ix == 15:
+            break
+        name.append(i)
+    return "".join(name)
+
+
+@dataclass(sorted=True)
 class UserMinecraft:
-    name: str
+    name: str = generate_random_username()
     Offline: bool
+    Vanilla: bool
     Forge: bool
     Fabric: bool
+    uuid: str
+
     def __init__(self):
-        self.uuid = uuid.uuid3(uuid.RESERVED_MICROSOFT, self.name)
-
-    def __repr__(self):
-        return f"UserMinecraft(name={self.name}, Offline={self.Offline}, Forge={self.Forge}, Fabric={self.Fabric})"
-    
-
-        
+        # correct values if possible
+        if self.Vanilla:
+            self.Forge = False
+            self.Fabric = False
+        elif self.Forge:
+            self.Vanilla = False
+            self.Fabric = False
+        elif self.Fabric:
+            self.Vanilla = False
+            self.Forge = False
+        self.uuid = str(uuid.uuid3(uuid.RESERVED_MICROSOFT, self.name))
