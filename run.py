40a105c3fec25eb91ae02773c1e5251594bd616f
Yahya-Amrati <y.amrati.code@gmail.com>
2025-02-16 19:57:10 +0100

    refactor: update path resolution to use local path from Utils_minecraft

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-import streamlit.web.cli as stcli
-import sys, os
+import sys
+import os
+import Utils_minecraft
 import dependecies
-
-def resolve_path(path):
-    return os.path.abspath(os.path.join(os.getcwd(), path))
+import streamlit.web.cli as stcli
 
 if __name__ == "__main__":
     dependecies.check()
+    def resolve_path(path):
+        return os.path.abspath(os.path.join(Utils_minecraft.local_path(), path))
     sys.argv = ["streamlit", "run", resolve_path("UI.py"), "--global.developmentMode=false"]
     sys.exit(stcli.main())
